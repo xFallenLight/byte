36edf9970398837a598b4d168aad9b79799f43e3
Kyle <31027460+dvhe@users.noreply.github.com>
2017-12-08 20:37:39 -0500

    checks + works for me
    https://a.pomf.cat/lfipfw.png

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`[READY] Logged in as ${client.user.tag}`);
-  client.user.setGame(`Type ${prefix}cmds to get started.`)
+  client.user.setGame(`Type ${prefix}cmds to get started.`);
 });
 
-let prefix = "b:"
+let prefix = "b:";
 
 client.on('message', msg => {
     if (msg.content.startsWith(prefix + 'ping')) {
         msg.channel.send("Pinging...").then(sent => {
-            sent.edit(`Pong! \`${sent.createdTimestamp - msg.createdTimestamp}ms\``)
-        })
+            sent.edit(`Pong! \`${sent.createdTimestamp - msg.createdTimestamp}ms\``);
+        });
     }
 
     if (msg.content.startsWith(prefix + 'say')) {
@@ -21,27 +21,32 @@ client.on('message', msg => {
             return msg.reply(`Please provide something for me to say.`);
         }
         return msg.channel.send(`${args}`);
-    } 
+    }
 
-    if (msg.content.startsWith(prefix + 'esay')) {
+     if (msg.content.startsWith(prefix + 'esay')) {
+        if(msg.channel.permissionsFor(client.user).has("EMBED_LINKS")) {
+            return msg.reply("I am unable to send embeds in this channel.");
+        }
+        
         let args = msg.content.split(' ').slice(1).join(' ');
         if (!args) {
-            return msg.reply(`Please provide something for me to embed.`);
+            return msg.reply(`Please provide something for embed.`);
         }
+        
         const embed = new Discord.RichEmbed()
         .setTitle(`Message from ${msg.author.tag}`)
         .setColor(`RANDOM`)
-        .setDescription(`${args}`)
+        .setDescription(args);
 
         return msg.channel.send({embed});
-    } 
+    }
 
     if (msg.content.startsWith(prefix + 'invite')) {
-        msg.reply(`Invite me using this URL:\nhttps://discordapp.com/oauth2/authorize/?permissions=8&scope=bot&client_id=388630510399782912.`)
+        msg.reply(`Invite me using this URL:\nhttps://discordapp.com/oauth2/authorize/?permissions=8&scope=bot&client_id=388630510399782912.`);
     }
 
     if (msg.content.startsWith(prefix + 'support')) {
-        msg.reply(`You can join my support server here:\nhttps://discord.gg/2AxaHpf.`)
+        msg.reply(`You can join my support server here:\nhttps://discord.gg/2AxaHpf.`);
     }
 
     if (msg.content.startsWith(prefix + 'eval')) {
@@ -62,7 +67,7 @@ client.on('message', msg => {
            .setTitle(`EVAL ✅`)
        
            .setColor("0x4f351")
-           .setDescription(`📥 Input: \n \`\`\`${evalstuff}\`\`\` \n 📤 Output: \n  \`\`\`${clean(evaled)}\`\`\``)
+           .setDescription(`📥 Input: \n \`\`\`${evalstuff}\`\`\` \n 📤 Output: \n  \`\`\`${clean(evaled)}\`\`\``);
        
          msg.channel.send({embed});
        } catch (err) {
@@ -70,7 +75,7 @@ client.on('message', msg => {
          .setTitle(`EVAL ❌`)
     
          .setColor("0xff0202")
-         .setDescription(`📥 Input: \n \`\`\`${evalstuff}\`\`\` \n 📤 Output: \n  \`\`\`${clean(err)}\`\`\``)
+         .setDescription(`📥 Input: \n \`\`\`${evalstuff}\`\`\` \n 📤 Output: \n  \`\`\`${clean(err)}\`\`\``);
     
          msg.channel.send({embed});
        }
@@ -78,14 +83,14 @@ client.on('message', msg => {
   
     if (msg.content === "<@388630510399782912> prefix") {
       return msg.reply(`My prefix is: \`${prefix}\`.`);
-    }
+    } else if(msg.content === "<@!388630510399782912> prefix") // this is for if the bot has a nickname.
 
     if (msg.content.startsWith(prefix + 'cmds')) {
         const embed = new Discord.RichEmbed()
         .setTitle(`Commands`)
         .setColor(`RANDOM`)
         .setDescription(`\`${prefix}cmds\`\n\`${prefix}esay [arguments]\`\n\`${prefix}invite\`\n\`${prefix}ping\`\n\`${prefix}say [arguments]\`\n\`${prefix}support\``)
-        .setFooter(`${prefix}esay & ${prefix}say have been disabled. There's nothing wrong with the say command but the esay command is messed up.`)
+        .setFooter(`${prefix}esay & ${prefix}say have been disabled. There's nothing wrong with the say command but the esay command is messed up.`);
 
         msg.channel.send({embed});
     }
